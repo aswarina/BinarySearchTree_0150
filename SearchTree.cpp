c6ec82722ccd4aba81ad28387c62eacd06ede487
aswarina <lyxsyyawa@gmail.com>
2026-06-11 11:35:31 +0700

    mmbuat case 2 untuk menampilkan data secara inorder

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -167,5 +167,11 @@ int main()
             obj.insert(word);
             break;
         }
+
+        case '2':
+        {
+            obj.inorder(obj.ROOT);
+            break;
+        }
             
 
